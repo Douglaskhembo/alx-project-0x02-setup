c5b339f990017821aa7fa5ac47164a7d176d1a3c
khembo <khembo.douglas.juma@gmail.com>
2025-06-05 00:37:52 +0300

    Create and Use a Button Component

--- alx-project-0x02/components/common/PostModal.tsx
+++ alx-project-0x02/components/common/PostModal.tsx
@@ -40,14 +40,21 @@ const PostModal: React.FC<PostModalProps> = ({ isOpen, onClose, onSubmit }) => {
             className="w-full mb-3 p-2 border rounded"
             required
           />
-          <div className="flex justify-end gap-2">
-            <button type="button" onClick={onClose} className="px-4 py-2 bg-gray-300 rounded">
-              Cancel
+            <div className="flex justify-end gap-2">
+            <button
+                type="button"
+                onClick={onClose}
+                className="px-4 py-2 bg-red-500 rounded cursor-pointer hover:bg-red-600 hover:font-bold transition duration-200">
+                Cancel
             </button>
-            <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">
-              Post
+            <button
+                type="submit"
+                disabled={!title || !content}
+                className="px-4 py-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600 hover:font-bold transition duration-200 disabled:opacity-50">
+                Post
             </button>
-          </div>
+            </div>
+
         </form>
       </div>
     </div>
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,10 +1,16 @@
+import Button from "@/components/common/Button";
+
 const About = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold">About Us</h1>
-      <p>Welcome to our about page!</p>
-      <p>We are dedicated to providing the best service possible.</p>
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">About Page</h1>
+      <div className="flex gap-4 flex-wrap">
+        <Button label="Small Button" size="small" shape="rounded-sm" />
+        <Button label="Medium Button" size="medium" shape="rounded-md" />
+        <Button label="Large Button" size="large" shape="rounded-full" />
+      </div>
     </div>
   );
-}
+};
+
 export default About;
