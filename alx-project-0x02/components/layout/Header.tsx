2816ce72b671d5965586ec9a3f57607db2a8e95e
khembo <khembo.douglas.juma@gmail.com>
2025-06-04 22:44:52 +0300

    Basic Routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
+
 const Header = () => {
     return(
-        <header className="bg-blue-600 text-white p-4">
+        <header className="bg-blue-600 text-white p-4 flex justify-between">
             <h1 className="text-xl font-bold">Welcome To Airbnb Clone Project</h1>
+            <nav>
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link href="/home" className="hover:underline">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className="hover:underline">About</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p>Welcome to our about page!</p>
+      <p>We are dedicated to providing the best service possible.</p>
+    </div>
+  );
+}
+export default About;
