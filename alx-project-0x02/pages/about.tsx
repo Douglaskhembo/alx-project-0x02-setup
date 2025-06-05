a1d49858d22f1e78b80d5b9b590b414852d09a1e
khembo <khembo.douglas.juma@gmail.com>
2025-06-05 21:12:31 +0300

    modified

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,15 +1,19 @@
+import Header from "@/components/layout/Header";
 import Button from "@/components/common/Button";
 
 const About = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">About Page</h1>
-      <div className="flex gap-4 flex-wrap">
-        <Button label="Small Button" size="small" shape="rounded-sm" />
-        <Button label="Medium Button" size="medium" shape="rounded-md" />
-        <Button label="Large Button" size="large" shape="rounded-full" />
+    <>
+      <Header />
+      <div className="p-8">
+        <h1 className="text-2xl font-bold mb-4">About Page</h1>
+        <div className="flex gap-4 flex-wrap">
+          <Button label="Small Button" size="small" shape="rounded-sm" />
+          <Button label="Medium Button" size="medium" shape="rounded-md" />
+          <Button label="Large Button" size="large" shape="rounded-full" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Header from "@/components/layout/Header";
 import Card from "@/components/common/Card";
 import PostModal from "@/components/common/PostModal";
 
@@ -13,27 +14,30 @@ const Home = () => {
   };
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Home Page</h1>
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="mb-6 px-4 py-2 bg-green-600 text-white rounded"
-      >
-        Add New Post
-      </button>
+    <>
+      <Header />
+      <div className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Home Page</h1>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="mb-6 px-4 py-2 bg-green-600 text-white rounded hover:font-bold hover:text-lg cursor-pointer"
+        >
+          Add New Post
+        </button>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {cards.map((card, index) => (
-          <Card key={index} title={card.title} content={card.content} />
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {cards.map((card, index) => (
+            <Card key={index} title={card.title} content={card.content} />
+          ))}
+        </div>
+
+        <PostModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onSubmit={handleAddCard}
+        />
       </div>
-
-      <PostModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSubmit={handleAddCard}
-      />
-    </div>
+    </>
   );
 };
 
