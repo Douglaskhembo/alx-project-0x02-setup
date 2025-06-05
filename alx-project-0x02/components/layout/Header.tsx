545c8c9748b9b538558c3b827f0c085c8d5cdde2
khembo <khembo.douglas.juma@gmail.com>
2025-06-05 21:04:23 +0300

    creating posts

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,21 +1,16 @@
 import Link from "next/link";
 
-
 const Header = () => {
-    return(
-        <header className="bg-blue-600 text-white p-4 flex justify-between">
-            <h1 className="text-xl font-bold">Welcome To Airbnb Clone Project</h1>
-            <nav>
-                <ul className="flex space-x-4">
-                    <li>
-                        <Link href="/home" className="hover:underline">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/about" className="hover:underline">About</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold">ALX Project</h1>
+      <nav className="space-x-4">
+        <Link href="/home" className="hover:underline hover:font-semibold">Home</Link>
+        <Link href="/about" className="hover:underline hover:font-semibold">About</Link>
+        <Link href="/posts" className="hover:underline hover:font-semibold">Posts</Link>
+      </nav>
+    </header>
+  );
 };
+
 export default Header;
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+
+export default function Posts() {
+  return (
+    <>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Posts Page</h1>
+        {/* You can dynamically show posts here later */}
+      </main>
+    </>
+  );
+}
